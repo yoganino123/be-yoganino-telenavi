ecfe053dbae12f7b6cb6d9b7692505d7cb76f34b
Rahardian Yoganino Pradipta <yoganinorahardian@gmail.com>
2025-09-18 01:53:26 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['status' => 'OK', 'message' => 'Welcome']);
 });
 
 Route::get('/healthz', function () {
